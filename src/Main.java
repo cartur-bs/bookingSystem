5fed5ac4973853ebb2b5b36e8c882b42745bab76
Carlos Artur Brito da Silva <carlosarturbritodasilva@MacBook-Air-de-Carlos.local>
2023-12-19 19:11:32 -0300

    aditional test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hey there");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hey there, what's your name?");
+        String name = sc.next();
+        System.out.println("Hey," + name);
+        sc.close();
     }
 }
